7205f1af51172b6d09e0c784f6e4785f82893058
Thamiliniyan Aravinthan <aravintt@roehampton.ac.uk>
2022-03-22 10:20:40 +0000

    fixes to code and comments

--- app/server.js
+++ app/server.js
@@ -1,15 +1,18 @@
 // get sql module
 const express = require('express');
 
+// Create express app
 var app = express();
 
+// Get functions fro db.js file
 const db = require('./services/db');
 
+// Route for root
 app.get("/", function(req, res) {
   res.send("Main Page");
 });
 
-// If URL contains get citiies
+// If route contains get citiies
 app.get('/getcities', function(req,res) {
   // Variable sql that contains instructions to query the database
     var sql = 'SELECT * FROM city ';
@@ -17,58 +20,62 @@ app.get('/getcities', function(req,res) {
     db.query(sql).then(results => {
         // Prints data in terminal 
         console.log(results);
-        // Displays data from database
+        // Displays data to browser
         res.json(results);
     });
 })
 
-// If URL contains get contries
+// If route contains get contries
 app.get('/getcountries', (req,res) => {
   // Variable sql that contains instructions to query the database
-    let sql = 'SELECT * FROM country';
+    var sql = 'SELECT * FROM country';
     // Variable that queries the database using the sql variable
-    db.query(sql, (err, results) => {
-      // Throws error if there is one
-        if(err) throw err;
+    db.query(sql).then(results => {
         // Prints data in terminal
         console.log(results);
-        // Displays data from database
-        res.send(results);
+        // Displays data to browser
+        res.json(results);
     });
 })
 
-// If URL contains get language
+// If route contains get language
 app.get('/getcountrylanguage', (req,res) => {
-  // Variable sql that contains instructions to query the database
-    let sql = 'SELECT * FROM countrylanguage';
+    // Variable sql that contains instructions to query the database
+    var sql = 'SELECT * FROM countrylanguage';
     // Variable that queries the database using the sql variable
-    db.query(sql, (err, results) => {
-      // Throws error if there is one
-        if(err) throw err;
+    db.query(sql).then(results => {
         // Prints data in terminal
         console.log(results);
-        // Displays data from database
-        res.send(results);
+        // Displays data to browser
+        res.json(results);
     });
 })
 
+// If route contains country pop descending
 app.get('/country-pop-descending', (req,res) =>{
-    let sql = 'SELECT * FROM country ORDER BY Population DESC'
+    // Variable sql that contains instructions to query the database
+    var sql = 'SELECT * FROM country ORDER BY Population DESC'
+    // Variable to print table and append data
     var output = '<table border="lpx">';
-    db.query(sql, (err, results) => {
+    // Query the database
+    db.query(sql).then(results => {
+      // Loop and iterate throw every row
       for (var row of results) {
+        // Format table and append data
         output += '<tr>';
         output += '<td>' + row.Name + '<td>';
         output += '<td>' + row.Population + '<td>';
         output += '</tr>'
     }
+    // Close table
     output += '</table>';
-    res.send(output);
+    // Send out output
+    res.json(output);
 });
 })
 
 // Choose port 3000 for the server to run on
 app.listen('3000', () => {
-  // Print message once connection is made
-console.log('Server running at http://127.0.0.1:3000/');
+// Print message once connection is made
+console.log('Server running at http://localhost:3000/');
 });
--- app/services/db.js
+++ app/services/db.js
@@ -1,27 +1,31 @@
 // Create a const variable that will connect with database
 const mysql = require('mysql2/promise');
 
+// Create config variable that is constant to hold db info
 const config = {
     db: {
-    host: 'localhost',
+    host: process.env.DB_HOST || 'localhost',
     user: 'root',
-    password: '',
+    password: process.env.DB_PASSWORD || '',
     database: 'world',
-    port:'3308',
+    port:'3306',
     waitForConnections: true,
     connectionLimit: 2,
     queueLimit: 0,
     },
   };
   
+  // Create pool using config data
   const pool = mysql.createPool(config.db);
   
+  // Async function to query database and wait for data to be read
   async function query(sql, params) {
     const [rows, fields] = await pool.execute(sql, params);
-  
+    // Will return the row values
     return rows;
   }
 
+  // Export the query function
   module.exports = {
       query,
   }

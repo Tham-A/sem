0bc770a8fe903b0a80ae21175289c436e4f2caf2
Ygolam <98460074+Ygolam@users.noreply.github.com>
2022-04-17 19:33:18 +0100

    Unit Testing files and packages

---
+++ app/__test__/input.test.js
@@ -0,0 +1,43 @@
+const mysql = require('mysql2/promise');
+const testQuery = async query => {
+
+    const config = {
+        db: {
+        host: process.env.DB_HOST || 'localhost',
+        user: 'root',
+        password: process.env.DB_PASSWORD || '',
+        database: 'world',
+        port:'3306',
+        waitForConnections: true,
+        connectionLimit: 2,
+        queueLimit: 0,
+        },
+      };
+    const pool = mysql.createPool(config.db)
+}
+
+it('Should return getPopulationContinent query', async () => {
+    const recordset = await testQuery(`
+        SELECT country.Continent AS name, SUM(country.Population) AS Pop, SUM(city.Population) \
+        AS InCities, ((SUM(country.Population)-SUM(city.Population))) AS OutCities FROM country, city WHERE \
+        city.CountryCode = country.Code GROUP BY country.Continent ORDER BY Pop DESC; 
+    `)
+    expect(recordset).toEqual(undefined)
+  })
+
+it('Should return getCountryRegPop query', async () => {
+    const recordset = await testQuery(`
+    SELECT * FROM country ORDER BY Region, Population DESC;; 
+    `)
+    expect(recordset).toEqual(undefined)
+  })
+
+it('Should return getCountryPop query', async () => {
+    const recordset = await testQuery(`
+    SELECT * FROM country ORDER BY Population DESC;; 
+    `)
+    expect(recordset).toEqual(undefined)
+  })
+
+
+  

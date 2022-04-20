e744d33393a16f3f5551841fdeb49b76e54f03c8
Ygolam <98460074+Ygolam@users.noreply.github.com>
2022-04-20 17:14:00 +0100

    Added some tests

--- app/__test__/input.test.js
+++ app/__test__/input.test.js
@@ -16,7 +16,7 @@ const testQuery = async query => {
     const pool = mysql.createPool(config.db)
 }
 
-it('Should return getPopulationContinent query', async () => {
+test('Should return getPopulationContinent query', async () => {
     const recordset = await testQuery(`
         SELECT country.Continent AS name, SUM(country.Population) AS Pop, SUM(city.Population) \
         AS InCities, ((SUM(country.Population)-SUM(city.Population))) AS OutCities FROM country, city WHERE \
@@ -25,16 +25,39 @@ it('Should return getPopulationContinent query', async () => {
     expect(recordset).toEqual(undefined)
   })
 
-it('Should return getCountryRegPop query', async () => {
+test('Should return getCountryRegPop query', async () => {
     const recordset = await testQuery(`
-    SELECT * FROM country ORDER BY Region, Population DESC;; 
+    SELECT * FROM country ORDER BY Region, Population DESC; 
     `)
     expect(recordset).toEqual(undefined)
   })
 
-it('Should return getCountryPop query', async () => {
+test('Should return getCountryPop query', async () => {
     const recordset = await testQuery(`
-    SELECT * FROM country ORDER BY Population DESC;; 
+    SELECT * FROM country ORDER BY Population DESC; 
+    `)
+    expect(recordset).toEqual(undefined)
+  })
+
+test('Should return getCountryContPopUser query', async () => {
+    const recordset = await testQuery(`
+    SELECT * FROM country ORDER BY Continent, Population DESC LIMIT ?; 
+    `)
+    expect(recordset).toEqual(undefined)
+  })
+
+test('Should return getCitiesPopUser query', async () => {
+    const recordset = await testQuery(`
+    SELECT * FROM city ORDER BY Population DESC LIMIT ?; 
+    `)
+    expect(recordset).toEqual(undefined)
+  })
+
+test('Should return getCitiesRegPopUser query', async () => {
+    const recordset = await testQuery(`
+    SELECT DISTINCT city.* FROM city INNER JOIN country ON \
+    city.CountryCode = country.Code ORDER BY country.Region, \
+    city.Population DESC LIMIT ?; 
     `)
     expect(recordset).toEqual(undefined)
   })
